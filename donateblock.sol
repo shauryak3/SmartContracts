d1ac230833c303baa18b5c2d5ed3b8158ac04657
shauryak3 <shauryakalia@gmail.com>
2018-11-15 10:19:47 +0530

    basic contract structure

--- donateblock.sol
+++ donateblock.sol
@@ -3,8 +3,18 @@ pragma solidity ^0.4.16;
 
 contract DonateBlockCampaign {
     address public organization;
+    address[] public donors;
+    address[] public selectedVendors;
+    int public 
 
     function DonateBlockCampaign() public {
         organization = msg.sender;
     }
+
+    function donate() public payable{
+        require(msg.value > 0.01 ether);
+        donors.push(msg.sender);
+    }
+
+
 }
--- lottery.sol
+++ lottery.sol
@@ -3,8 +3,25 @@ pragma solidity ^0.4.16;
 
 contract Lottery {
     address public manager;
+    address[] public players;
 
     function Lottery() public {
         manager = msg.sender;
     }
+
+    function enter() public payable {
+        require(msg.value > 0.01 ether);
+        players.push(msg.sender);
+    }
+
+    function pickWinner() public {
+        uint index = random() % players.length;
+        players[index].transfer(address(this).balance);
+        players = new address[](0);
+    }
+
+    function random() private view returns (uint) {
+        return uint(keccak256(block.difficulty, now, players));
+    }
+ 
 }
